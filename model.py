71c381878aea82bdee67ba011d5eb09d8020d4ff
justwon322 <justwon322@gmail.com>
2021-10-05 23:21:55 +0900

    detection + recognition 합침
    코드 정리는 추후에 예정 일단 돌아가는 detection(EAST모델) + recogniton(AI_hub) 합침

--- model.py
+++ model.py
@@ -3,7 +3,10 @@ import torch.nn as nn
 import torch.utils.model_zoo as model_zoo
 import torch.nn.functional as F
 import math
-
+from modules.transformation import TPS_SpatialTransformerNetwork
+from modules.feature_extraction import VGG_FeatureExtractor, RCNN_FeatureExtractor, ResNet_FeatureExtractor
+from modules.sequence_modeling import BidirectionalLSTM
+from modules.prediction import Attention
 
 cfg = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
 
@@ -166,7 +169,52 @@ class EAST(nn.Module):
 	
 	def forward(self, x):
 		return self.output(self.merge(self.extractor(x)))
-		
+
+
+
+class detectionModel(nn.Module):
+
+    def __init__(self, num_class):
+        super(detectionModel, self).__init__()
+        self.num_class = num_class
+        self.stages = {'Trans': True, 'Feat': True,
+                       'Seq': True, 'Pred': True}
+
+        """ Transformation """
+        self.Transformation = TPS_SpatialTransformerNetwork( # https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=worb1605&logNo=221580830661
+            F=20, I_size=(32, 100), I_r_size=(32, 100), I_channel_num=1)
+
+        """ FeatureExtraction """
+        self.FeatureExtraction = ResNet_FeatureExtractor(1, 512)
+        self.FeatureExtraction_output = 512  # int(imgH/16-1) * 512
+        self.AdaptiveAvgPool = nn.AdaptiveAvgPool2d((None, 1))  # Transform final (imgH/16-1) -> 1
+
+        """ Sequence modeling"""
+        self.SequenceModeling = nn.Sequential(
+            BidirectionalLSTM(self.FeatureExtraction_output, 256, 256),
+            BidirectionalLSTM(256, 256, 256))
+        self.SequenceModeling_output = 256
+        """ Prediction """
+        self.Prediction = Attention(self.SequenceModeling_output, 256, num_class)
+
+    def forward(self, input, text, is_train=True):
+        """ Transformation stage """
+        if not self.stages['Trans'] == "None":
+            input = self.Transformation(input)
+
+        """ Feature extraction stage """
+        visual_feature = self.FeatureExtraction(input)
+        visual_feature = self.AdaptiveAvgPool(visual_feature.permute(0, 3, 1, 2))  # [b, c, h, w] -> [b, w, c, h]
+        visual_feature = visual_feature.squeeze(3)
+
+        """ Sequence modeling stage """
+        contextual_feature = self.SequenceModeling(visual_feature)
+
+        """ Prediction stage """
+        prediction = self.Prediction(contextual_feature.contiguous(), text, is_train, batch_max_length=25)
+
+        return prediction
+
 
 if __name__ == '__main__':
 	m = EAST()
